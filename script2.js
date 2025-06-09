52d66505ca1d66a3b3093eda75d8274d65066aaa
Jose Gilmar <jose.gilmar05@aluno.ifce.edu.br>
2025-06-09 20:39:16 -0300

    Criar script2.js

---
+++ script2.js
@@ -0,0 +1,3 @@
+// Adicione funcionalidades JavaScript aqui, se necessário
+// Por exemplo, você pode adicionar interatividade futura, como um botão para alternar temas ou animações
+console.log("Página Sobre Mim carregada!");
